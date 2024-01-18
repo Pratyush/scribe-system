16159a64c34eef3aed2b017e9041cfb3844fdf12
Pratyush Mishra <pratyushmishra@berkeley.edu>
2024-01-18 09:53:15 -0500

    Tweak

--- src/space_prover.rs
+++ src/space_prover.rs
@@ -43,7 +43,6 @@ impl<F: Field> Prover<F> for SpaceProver<F> {
         let one = F::ONE;
         let mut a_i0 = F::zero();
         let mut a_i1 = F::zero();
-        // Previous round's update
         while let (Some(a_even_0), Some(a_odd_0), Some(a_even_1), Some(a_odd_1)) = (
             self.stream.read_next(),
             self.stream.read_next(),
@@ -51,10 +50,12 @@ impl<F: Field> Prover<F> for SpaceProver<F> {
             self.stream.read_next(),
         ) {
             if let Some(challenge) = self.challenges.last() {
+                // Compute previous round update
                 let temp_0 = a_even_0 * (one - challenge) + a_odd_0 * challenge;
                 let temp_1 = a_even_1 * (one - challenge) + a_odd_1 * challenge;
                 self.stream.write_next(temp_0);
                 self.stream.write_next(temp_1);
+                // Compute current round message
                 a_i0 += temp_0;
                 a_i1 += temp_1;
                 self.stream.swap_read_write();
