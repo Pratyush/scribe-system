4983bec145dc3796b67a232e556e7d37b5f32410
Pratyush Mishra <pratyushmishra@berkeley.edu>
2024-04-17 15:00:34 -0400

    Work

--- src/streams/file_vec/macros.rs
+++ src/streams/file_vec/macros.rs
@@ -1,7 +1,6 @@
 macro_rules! process_file {
     ($self:ident, $extra:expr) => {{
         let mut reader = BufReader::new(&mut $self.file);
-        reader.rewind().expect("failed to rewind file");
         let mut buffer = Vec::with_capacity(BUFFER_SIZE);
         let tmp = NamedTempFile::new().expect("failed to create temp file");
         let mut writer = BufWriter::new(tmp);
@@ -13,6 +12,7 @@ macro_rules! process_file {
                     Err(_) => break,
                 }
             }
+
             if buffer.is_empty() {
                 break;
             }
@@ -20,6 +20,7 @@ macro_rules! process_file {
             if $extra(&mut buffer).is_none() {
                 break;
             }
+
             for item in &buffer {
                 item.serialize_uncompressed(&mut writer).expect("failed to write to file");
             }
@@ -29,5 +30,6 @@ macro_rules! process_file {
         let (new_file, new_path) = new_file.keep().expect("failed to keep temp file");
         $self.path = new_path;
         $self.file = new_file;
+        $self.file.rewind().expect("could not rewind file");
     }};
 }
