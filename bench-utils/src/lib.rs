9af3ac766b8f58dc4c4e97f395eb2bf0a83e07b3
Pratyush Mishra <pratyushmishra@berkeley.edu>
2024-09-09 08:11:52 -0700

    Clean up

--- bench-utils/src/lib.rs
+++ bench-utils/src/lib.rs
@@ -18,7 +18,7 @@ pub fn setup(min_num_vars: usize, max_num_vars: usize) {
     let mut rng = test_rng();
     let pc_srs = PST13::<Bls12_381>::gen_fake_srs_for_testing(&mut rng, max_num_vars).unwrap();
 
-    let srs_filename = format!("srs_{}.params", max_num_vars);
+    let srs_filename = format!("srs_{max_num_vars}.params");
     let mut srs_file = OpenOptions::new()
         .write(true)
         .create(true)
@@ -26,12 +26,10 @@ pub fn setup(min_num_vars: usize, max_num_vars: usize) {
         .unwrap();
     pc_srs.serialize_uncompressed(&mut srs_file).unwrap();
 
-    // println!("srs powers of g: {}", pcs_srs.prover_param.powers_g.len());
-
     // generate and serialize circuit, pk, vk
-    let circuit_filename = format!("circuit_{}_to_{}.params", min_num_vars, max_num_vars);
-    let pk_filename = format!("pk_{}_to_{}.params", min_num_vars, max_num_vars);
-    let vk_filename = format!("vk_{}_to_{}.params", min_num_vars, max_num_vars);
+    let circuit_filename = format!("circuit_{min_num_vars}_to_{max_num_vars}.params");
+    let pk_filename = format!("pk_{min_num_vars}_to_{max_num_vars}.params");
+    let vk_filename = format!("vk_{min_num_vars}_to_{max_num_vars}.params");
     let circuit_file = OpenOptions::new()
         .write(true)
         .create(true)
@@ -53,7 +51,6 @@ pub fn setup(min_num_vars: usize, max_num_vars: usize) {
 
     let vanilla_gate = CustomizedGates::vanilla_plonk_gate();
     for nv in min_num_vars..=max_num_vars {
-        println!("nv = {}", nv);
         let circuit = MockCircuit::<Fr>::new(1 << nv, &vanilla_gate);
 
         circuit.serialize_uncompressed(&circuit_file).unwrap();
@@ -70,49 +67,6 @@ pub fn setup(min_num_vars: usize, max_num_vars: usize) {
         ProvingKey::<_, PST13<_>>::serialize_uncompressed(&pk, &pk_file).unwrap();
         VerifyingKey::<_, PST13<_>>::serialize_uncompressed(&vk, &vk_file).unwrap();
     }
-
-    println!("Serializing Circuit");
-    let mut circuit_file = OpenOptions::new()
-        .read(true)
-        .open(&circuit_filename)
-        .unwrap();
-    println!("Serializing PK");
-    let mut pk_file = OpenOptions::new().read(true).open(&pk_filename).unwrap();
-    println!("Serializing VK");
-    let _vk_file = OpenOptions::new().read(true).open(&vk_filename).unwrap();
-
-    let circuit_2 =
-        MockCircuit::<Fr>::deserialize_uncompressed_unchecked(&mut circuit_file).unwrap();
-
-    println!("circuit");
-    println!("pub inputs: {:?}", circuit_2.public_inputs);
-    circuit_2
-        .witnesses
-        .iter()
-        .for_each(|witness| println!("witness: {witness}"));
-    println!("params: {:?}", circuit_2.index.params);
-    circuit_2
-        .index
-        .permutation
-        .iter()
-        .for_each(|perm| println!("permutation: {perm}"));
-    circuit_2
-        .index
-        .selectors
-        .iter()
-        .for_each(|selector| println!("selector: {selector}"));
-
-    println!("pk 2");
-    let pk_2 = ProvingKey::<_, PST13<Bls12_381>>::deserialize_uncompressed_unchecked(&mut pk_file)
-        .unwrap();
-    println!("{:?}", pk_2.params);
-    assert_eq!(pk_2.params, circuit_2.index.params);
-    pk_2.permutation_oracles
-        .iter()
-        .for_each(|perm| println!("permutation: {perm}"));
-    pk_2.selector_oracles
-        .iter()
-        .for_each(|selector| println!("selector: {selector}"));
 }
 
 pub fn prover(min_num_vars: usize, max_num_vars: usize) -> Result<(), ScribeErrors> {
