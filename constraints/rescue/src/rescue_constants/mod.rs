0b1e5f38a6bcb068347c6b8bc9885ab5e4c7e101
Pratyush Mishra <pratyushmishra@berkeley.edu>
2024-11-14 17:48:00 -0800

    Remove unused constraints

--- constraints/rescue/src/rescue_constants/mod.rs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2022 Espresso Systems (espressosys.com)
-// This file is part of the Jellyfish library.
-
-// You should have received a copy of the MIT License
-// along with the Jellyfish library. If not, see <https://mit-license.org/>.
-
-mod ed_on_bls12_377_base;
-mod ed_on_bls12_381_base;
-mod ed_on_bn254_base;
