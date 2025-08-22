e28009b095076285f6b81760d254e4013b3dbf78
Cloud User <ec2-user@ip-10-128-183-134.ec2.internal>
2025-08-22 19:47:13 +0000

    Update gemini-prover to use MiMalloc

--- bench-utils/examples/gemini-prover.rs
+++ bench-utils/examples/gemini-prover.rs
@@ -1,3 +1,7 @@
+use mimalloc::MiMalloc;
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
+
 pub fn main() {
     let args = std::env::args().skip(1).collect::<Vec<String>>();
     let min_num_vars: usize = args[0].parse().unwrap();
--- bench-utils/examples/scribe-prover.rs
+++ bench-utils/examples/scribe-prover.rs
@@ -2,9 +2,6 @@ use mimalloc::MiMalloc;
 #[global_allocator]
 static GLOBAL: MiMalloc = MiMalloc;
 
-/* use jemallocator::Jemalloc;
-
-*/
 pub fn main() {
     let args = std::env::args().skip(1).collect::<Vec<String>>();
     let min_num_vars: usize = args[0].parse().unwrap();
